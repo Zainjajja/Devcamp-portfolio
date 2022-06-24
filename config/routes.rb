5c295dbc29b0370fa0c97f9cd4a0afb725f346d1
zainjajja133@gmail.com <you@example.com>
2022-06-24 06:49:31 -0400

    Added the edit and update functionality from scratch

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   get 'pages/home'
   get 'pages/about'
   get 'pages/contact'
-  put 'pages/show'
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
