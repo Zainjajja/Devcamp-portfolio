b71796384a9bcd6236ef41c3a2ee434998b94735
zainjajja133@gmail.com <you@example.com>
2022-06-24 04:19:19 -0400

    build the new and create function from scratch

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,32 @@
 class PortfoliosController < ApplicationController
+    before_action :set_portfolio, only: %i[ show edit update destroy ]
+
 	def index
-    @portfolios = Portfolio.all
-  end
+        @portfolios = Portfolio.all
+    end
+
+    def new
+        @portfolios=Portfolio.new
+    end
+
+    def create
+        @portfolios=Portfolio.new(params.require(:portfolio).permit(:tittle, :subtittle,:body))
+
+        respond_to do |format|
+            if @portfolios.save
+            format.html { redirect_to (@portfolios), notice: "Portfolio was successfully created." }
+            format.json { render :show, status: :created, location: @portfolios}
+            end
+        end
+    end
+
+    def show
+
+    end
+
+    private
+
+    def set_portfolio
+        @portfolio = Portfolio.find_by_id(params[:id])
+    end
 end
